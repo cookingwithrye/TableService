5401264e2daad6a23211addb6f3c4c824e58c78f
Ryan Weir <ryweir@gmail.com>
2013-09-27 00:25:10 -0400

    initial structure for project

--- TableService/App_Start/RouteConfig.cs
+++ TableService/App_Start/RouteConfig.cs
@@ -14,10 +14,9 @@ namespace TableService
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "TableForOne",
+                url: "{controller}/{action}/{html}",
+                defaults: new { controller = "Waiter", action = "TableForOne" });
         }
     }
 }
---
+++ TableService/Controllers/WaiterController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TableService.Controllers
+{
+    public class WaiterController : Controller
+    {
+        //
+        // GET: /Waiter/
+
+        public ActionResult Index()
+        {
+            //send back sample page and explanation
+            return View();
+        }
+
+        //
+        // POST: /Waiter/TableForOne/"<html>...</html>"
+
+        //[HttpPost]
+        public ActionResult TableForOne(string html)
+        {
+            //TODO: parse the html, convert to tables, then return it
+            
+            ViewBag.TableHtml = html;
+            return View();
+        }
+    }
+}
